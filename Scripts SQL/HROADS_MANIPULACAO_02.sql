5e5f3f36c6486863e4becd6a4641a085e4bbc02a
Ariel Paixão Dos Santos <33747821839@info.local>
2019-02-04 15:24:16 -0200

    Criando pasta Scripts SQL

---
+++ Scripts SQL/HROADS_MANIPULACAO_02.sql
@@ -0,0 +1,10 @@
+use SENAI_HROADS_TARDE;
+
+insert into CLASSES	(NOME)
+	values			('Bárbaro'),
+					('Cruzado'),
+					('Caçadora de Demônios'),
+					('Monge'),
+					('Necromante'),
+					('Feiticeiro'),
+					('Arcanista')
